ca1e2feb7f2dd1113a9db67c5a1f9d984f2dfc82
Arfiansya <rachelnek12@gmail.com>
2024-02-21 11:48:15 +0700

    FrontEnd

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/page2.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="navbar">
+        <ul>
+            <li><a href="/homepage">Homepage</a></li>
+            <li><a href="#">Tentang</a></li>
+            <li><a href="#">Daftar Hotel</a></li>
+            <li><a href="#">Bantuan</a></li>
+        </ul>
+    </div>
+    <div class="kenalan">
+        <ul>
+            <li>Kenalan Lebih Dekat dengan
+Graha Wisata Kita dan Sejarahnya</li>
+        </ul>
+    </div>
+    <img src="{{ asset('Image/Graha.png') }}" class="logo" alt="">
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Graha Wisata Kita</title>
+    
+    <!--===style css====-->
+    <link rel="stylesheet" href="/css/menus.css">
+
+    <!--===google font===-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!--===navbar===-->
+    <nav>
+        <div class="kotak"></div>
+        <div class="kotak2"></div>
+        <div class="kotak3"></div>
+        <div class="navbar">
+            <ul>
+                <li><a href="/homepage">Homepage</a></li>
+                <li><a href="/index.php/id-tentang">Tentang</a></li>
+                <li><a href="#">Daftar Hotel</a></li>
+                <li><a href="#">Bantuan</a></li>
+            </ul>
+        </div> 
+    </nav>
+    <!--===hero section===-->
+    <div class="deskripsi2">
+        <ul>
+            <li>Membantu Menjalankan Operasi Bisnis Pariwisata Anda</li>
+        </ul>
+    </div>
+    <div class="deskripsi">
+        <ul>
+            <li>GWA membantu mengoperasikan keseluruhan layanan hotel, menajadikan mitra lebih percaya diri dalam menjalankan bisnis.</li>
+        </ul>
+    </div>
+    <div class="hotel">
+        <ul>
+            <li>HOTEL OPERATOR</li>
+        </ul>
+    </div>
+    <div style="height: 10px;">
+        <div class="mh-100" style="width: 23px; height: 5px;"></div>
+        <div class="mh-1002" style="width: 23px; height: 5px;"></div>
+    </div>
+    <img src="{{ asset('Image/Graha.png') }}" class="logo" alt="">
+    <div class="btn-group dropup" role="group"></div>
+    <div class="konsultasi"><a href="#">konsultasi Bisnis Saya</a></div>
+    <div class="gedung">
+        <img src="{{ asset('Image/Gedung.png') }}" class="gedung">
+        <div id="id-tentang" class="workflow">
+            <h4>OUR WORKFLOW</h4>
+            <h1>Proses Kerja Kami dalam Meningkatkan Kualitas Properti</h1>
+        </div>
+        <div class="ourservice">
+            <h4>OUR SERVICE</h4>
+            <h1>Proses Kerja Kami dalam Meningkatkan Kualitas Properti</h1>
+        </div>
+        <div class="rumusan">
+            <h1 id="id-rumusan">Rumusan</h1>
+            <ul>
+                <li>Setiap tantangan akan dirumuskan menjadi sebuah hal yang harus dicari jalan keluarnya oleh kami</li>
+            </ul>
+        </div>
+        <div class="tantangan">
+            <h1>Tantangan</h1>
+            <ul>
+                <li>Setiap property memiliki tantangan tersendiri dan GWA sangat memahami hal tersebut.</li>
+            </ul>
+        </div>
+        <div class="goals">
+            <h1>Goals</h1>
+            <ul>
+                <li>Dari rumusan masalah, kami memberikan respon cepat dan jalan keluar jangka pendek - menengah - panjang.</li>
+            </ul>
+        </div>
+        <div class="idea">
+            <h1>Ideas</h1>
+            <ul>
+                <li>Setiap rumusan menjadikan ide-ide kreatif bagi kami untuk meningkatkan kualitas property mitra..</li>
+            </ul>
+        </div>
+        <div class="service">
+            <h1>Revenue Management Service</h1>
+        </div>
+        <div class="manage">
+            <h1>Full Manage Service</h1>
+        </div>
+        <div class="monetize">
+            <h1>Asset Monetize Service</h1>
+        </div>
+        <div class="saya"><a href="#">konsultasi Bisnis Saya</a></div>
+        <div class="saya2"><a href="#">konsultasi Bisnis Saya</a></div>
+        <div class="saya3"><a href="/homepage">konsultasi Bisnis Saya</a></div>
+        <img src="{{ asset('Image/bendera.png') }}" class="bendera">
+        <img src="{{ asset('Image/tantangan.png') }}" class="rumusangambar">
+        <img src="{{ asset('Image/Goals.png') }}" class="goalsgambar">
+        <img src="{{ asset('Image/ideea.png') }}" class="ideagambar">
+        <img src="{{ asset('Image/Service.png') }}" class="servicegambar">
+        <img src="{{ asset('Image/manage.png') }}" class="managegambar">
+        <img src="{{ asset('Image/monetize.png') }}" class="monetizegambar">
+        <img src="{{ asset('Image/rumah.png') }}" class="rumahgambar">
+        <img src="{{ asset('Image/Client.png') }}" class="clientgambar">
+        <img src="{{ asset('Image/yellow.png') }}" class="yellowgambar">
+    </div>
+    <div class="card"></div>
+    <div class="project">
+        <h1>OUR LATEST PROJECTS</h1>
+    </div>
+    <div class="testimoni">
+        <h1>OUR TESTIMONIALS</h1>
+        <ul>
+            <li>GWA Group membantu hotel saya untuk menjangkau lebih banyak client dan memberikan saran monetisasi yang tidak pernah saya bayangkan sebelumnya</li>
+        </ul>
+        <div class="chaim">
+            <ul>
+                <li>Chaim Desmond</li>
+                <li>CEO of Yellow Hotel</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
